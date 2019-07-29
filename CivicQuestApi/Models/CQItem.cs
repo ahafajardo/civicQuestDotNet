a301a447e4a2f159a2533b7ea42449f27087f7f0
Aharon Fajardo <Aharon.Fajardo@arrowdigital.com>
2019-07-29 17:31:22 -0400

    initial commit of csproject

---
+++ CivicQuestApi/Models/CQItem.cs
@@ -0,0 +1,9 @@
+namespace CivicQuestApi.Models
+{
+    public class CQItem
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public bool isComplete { get; set; }
+    }
+}
