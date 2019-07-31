beee041fd39316784b0ab5eb97eac650d792eaee
Aharon Fajardo <Aharon.Fajardo@arrowdigital.com>
2019-07-31 12:37:49 -0400

    add and delete items on table now functional

--- CivicQuestApi/wwwroot/site.js
+++ CivicQuestApi/wwwroot/site.js
@@ -1,6 +1,7 @@
 const uri = "api/cq";
 const element = document.querySelector("#counter");
 const tableBody = document.querySelector("#todos");
+const addInput = document.querySelector("#add-name");
 
 let cqs = null;
 
@@ -18,20 +19,47 @@ function getData() {
       getCount(data.length);
 
       const tableRows = data.reduce(
-        (html, item) => `
-        <tr>
-            <td><input type="checkbox" ${item.isComplete ? "checked" : ""}/></td>
-            <td>${item.name}</td>
-            <td><button onclick="editItem(${item.id})">Edit</button></td>
-            <td><button onclick="deleteItem(${item.id})">Delete</button></td>
-        </tr>
+        (html, item) =>
+          html +
+          `
+            <tr>
+                <td><input type="checkbox" disabled="true" ${item.isComplete ? "checked" : ""}/></td>
+                <td>${item.name}</td>
+                <td><button onclick="editItem(${item.id})">Edit</button></td>
+                <td><button onclick="deleteItem(${item.id})">Delete</button></td>
+            </tr>
         `,
         ""
       );
 
       tableBody.innerHTML = tableRows;
       cqs = data;
-    });
+    })
+    .catch(err => console.error(err));
+}
+
+function addItem() {
+  const item = {
+    name: addInput.value,
+    isComplete: false
+  };
+
+  fetch(uri, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(item)
+  }).then(() => {
+    getData();
+    addInput.value = "";
+  });
+}
+
+function deleteItem(id) {
+  fetch(`${uri}/${id}`, {
+    method: "DELETE"
+  })
+    .then(() => getData())
+    .catch(err => console.error(err));
 }
 
 getData();
