2bdd61fb4750f360320ab55eb886aaf7661e0bd5
Aharon Fajardo <Aharon.Fajardo@arrowdigital.com>
2019-08-09 09:50:07 -0400

    reading all time entries from user and displaying them, hiding the add time entry form

--- CivicQuestApi/wwwroot/src/scripts/timesheets.js
+++ CivicQuestApi/wwwroot/src/scripts/timesheets.js
@@ -1,5 +1,9 @@
 const timeUri = "api/time";
 const returnUrl = "/";
+
+const timeEntries = document.querySelector(".time-entries");
+const addTimeEntry = document.querySelector(".time-entry--hidden");
+
 let timesheets = [];
 
 window.addEventListener("DOMContentLoaded", getTimesheets);
@@ -25,7 +29,44 @@ function getTimesheets() {
         timesheet.end = timesheet.end.split("T")[1];
         return timesheet;
       });
-      console.log(timesheets);
+      timesheets.forEach(timesheet => {
+        const timeEntryHTML = document.createElement("div");
+        timeEntryHTML.classList.add("time-entry");
+        timeEntryHTML.innerHTML = `
+        <div class="time-details">
+            <div class="time-stamps">
+              <div class="time-stamp">
+                <p class="time-stamp__text">Start</p>
+                <input type="text" class="input time-stamp__input" value="${
+  timesheet.start
+}" placeholder="00:00:00" disabled="true"/>
+              </div>
+              <div class="time-stamp">
+                <p class="time-stamp__text">End</p>
+                <input type="text" class="input time-stamp__input" value="${
+  timesheet.end
+}" placeholder="00:00:00" disabled="true"/>
+              </div>
+            </div>
+            <!-- /.time-stamps -->
+            <div class="time-info">
+              <label class="time-info__label">Notes</label>
+              <textarea class="input time-info__desc " placeholder="Write notes here..." disabled="true">${
+  timesheet.notes
+}</textarea>
+            </div>
+            <!-- /.time-info -->
+          </div>
+          <!-- /.time-details -->
+          <div class="time-actions">
+            <a href="#" class="btn btn--primary time-action">start</a>
+            <a href="#" class="btn btn--secondary time-action">edit</a>
+            <a href="#" class="btn btn--danger time-action">del</a>
+          </div>
+          <!-- /.time-actions -->
+        `;
+        timeEntries.appendChild(timeEntryHTML);
+      });
     })
     .catch(err => {
       localStorage.removeItem("token");
